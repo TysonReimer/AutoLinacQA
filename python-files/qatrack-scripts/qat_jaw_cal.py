1782ec3b25257c1873b73c762372f15b0a6ebe29
TysonReimer <38917827+TysonReimer@users.noreply.github.com>
2025-06-27 12:57:31 -0500

    Update qat_jaw_cal.py to correct any outlier pixels

--- python-files/qatrack-scripts/qat_jaw_cal.py
+++ python-files/qatrack-scripts/qat_jaw_cal.py
@@ -9,10 +9,32 @@ from pydicom import dcmread
 import zipfile
 import tempfile
 import io
+from skimage import morphology
+from scipy import ndimage as ndi
 from matplotlib.backends.backend_pdf import PdfPages
 
 
 ###############################################################################
+
+
+def suppress_hot_pixels(im, structure=np.ones((3, 3), bool)):
+    """ """
+    im = im.astype(float, copy=False)
+
+    hot_candidate = im > 0.3
+
+    isolated = hot_candidate & ~morphology.binary_opening(hot_candidate, structure)
+
+    if not isolated.any():
+        return im
+
+    kernel = np.ones((3, 3), float)
+    kernel[1, 1] = 0
+    neigh_mean = ndi.convolve(im, kernel, mode="reflect") / 8.0
+
+    im_fixed = im.copy()
+    im_fixed[isolated] = neigh_mean[isolated]
+    return im_fixed
 
 
 def _find_center_of_img(img):
@@ -31,13 +53,15 @@ def _find_center_of_img(img):
 
     # Normalize image
     img_normalized = (img - np.min(img)) / (np.max(img) - np.min(img))
-
+    img_normalized = suppress_hot_pixels(im=img_normalized)
+    img_normalized = (img_normalized - np.min(img_normalized)) / (
+        np.max(img_normalized) - np.min(img_normalized)
+    )
     # Find the center pixel
-    c_pix = np.argwhere(
-        img_normalized >= 0.5 * np.max(img_normalized)
-    ).mean(axis=0)
+    c_pix = np.argwhere(img_normalized >= 0.5 * np.max(img_normalized)).mean(axis=0)
 
     return c_pix
+
 
 def _find_CAX(ds_1, ds_2):
     """Find the MV CAX using two dicom files of jaw-defined field images
@@ -66,7 +90,6 @@ def _find_CAX(ds_1, ds_2):
     beam_CAX = beam_CAX[::-1]
 
     return beam_CAX
-
 
 
 def _idx_to_mm(ds, do_x, image, edge_pixel_position, beam_CAX):
@@ -101,15 +124,13 @@ def _idx_to_mm(ds, do_x, image, edge_pixel_position, beam_CAX):
 
     if do_x:
         # For an x-edge, convert the x coordinate relative to center:
-        physical_edge = ((edge_pixel_position - center_pixel[0])
-                         * pixel_spacing[1])
+        physical_edge = (edge_pixel_position - center_pixel[0]) * pixel_spacing[1]
         if beam_CAX is None:
             physical_edge += origin[0]
 
     else:  # If doing y
         # For a y-edge, convert the y coordinate relative to center:
-        physical_edge = ((edge_pixel_position - center_pixel[1])
-                         * pixel_spacing[0])
+        physical_edge = (edge_pixel_position - center_pixel[1]) * pixel_spacing[0]
         if beam_CAX is None:
             physical_edge += origin[1]
 
@@ -164,25 +185,22 @@ def _find_edge_in_slice(image, slice_index, from_negative, along_x, img_max):
         indices = np.where(np.flip(slice) >= threshold)[0]
 
     if indices.size != 0:  # If some indices were found
-
         idx = indices[0]
 
         # If the index is not at one of the extremes
         if (idx > 5) and (idx < len(slice) - 5):
-
             if from_negative:
                 # Linear interpolation between pixel idx-1 and idx:
-                frac = (threshold - slice[idx - 1]) / (
-                        slice[idx] - slice[idx - 1])
+                frac = (threshold - slice[idx - 1]) / (slice[idx] - slice[idx - 1])
                 edge_pos = (idx - 1) + frac
 
             else:  # If from positive axis
-
                 flip_slice = np.flip(slice)
 
                 # Linear interpolation between pixel idx-1 and idx:
                 frac = (threshold - flip_slice[idx - 1]) / (
-                        flip_slice[idx] - flip_slice[idx - 1])
+                    flip_slice[idx] - flip_slice[idx - 1]
+                )
                 edge_pos = (idx - 1) + frac
 
                 # Subtract 1 b/c 0-based indexing
@@ -191,8 +209,7 @@ def _find_edge_in_slice(image, slice_index, from_negative, along_x, img_max):
     return edge_pos
 
 
-def _find_edge(image, ds, img_max, along_x=True, from_negative=True,
-               beam_CAX=None):
+def _find_edge(image, ds, img_max, along_x=True, from_negative=True, beam_CAX=None):
     """Find the edge of the jaw-defined field in an EPID image
 
     Parameters
@@ -229,7 +246,6 @@ def _find_edge(image, ds, img_max, along_x=True, from_negative=True,
 
     # For each slice (excluding the edge cases)
     for slice_index in range(search_ini_idx, search_fin_idx):
-
         # Get the position of the edge in this slice
         edge_pos = _find_edge_in_slice(
             image,
@@ -319,10 +335,16 @@ def _find_all_edges(image, ds, beam_CAX, img_max):
         img_max=img_max,
     )
 
-    return (x1_edge, x1_edge_mm,
-            x2_edge, x2_edge_mm,
-            y1_edge, y1_edge_mm,
-            y2_edge, y2_edge_mm)
+    return (
+        x1_edge,
+        x1_edge_mm,
+        x2_edge,
+        x2_edge_mm,
+        y1_edge,
+        y1_edge_mm,
+        y2_edge,
+        y2_edge_mm,
+    )
 
 
 def _get_dcm_jaw_pos(ds):
@@ -346,17 +368,13 @@ def _get_dcm_jaw_pos(ds):
     """
 
     # Get x/y jaw positions
-    x_jaws = ds.ExposureSequence[0].BeamLimitingDeviceSequence[
-        0].LeafJawPositions
-    y_jaws = ds.ExposureSequence[0].BeamLimitingDeviceSequence[
-        1].LeafJawPositions
+    x_jaws = ds.ExposureSequence[0].BeamLimitingDeviceSequence[0].LeafJawPositions
+    y_jaws = ds.ExposureSequence[0].BeamLimitingDeviceSequence[1].LeafJawPositions
 
     return x_jaws[0], x_jaws[1], y_jaws[0], y_jaws[1]
 
 
 def _make_qat_dict_entry(jaw_dcm_pos, jaw_calc_pos, qat_dict, jaw_str):
-
-
     if "1" in jaw_str:
         cor_fac = -1
     else:
@@ -364,20 +382,13 @@ def _make_qat_dict_entry(jaw_dcm_pos, jaw_calc_pos, qat_dict, jaw_str):
 
     this_str = "%s %d" % (jaw_str, round(jaw_dcm_pos * cor_fac / 10, 0))
 
-
     if this_str in qat_dict.keys():
-
         # If a list
         if isinstance(qat_dict[this_str], list):
-
-            qat_dict[this_str] = (
-                    qat_dict[this_str] + [jaw_calc_pos * cor_fac]
-            )
+            qat_dict[this_str] = qat_dict[this_str] + [jaw_calc_pos * cor_fac]
 
         else:  # make it a list
-            qat_dict[this_str] = (
-                    [qat_dict[this_str]] + [jaw_calc_pos * cor_fac]
-            )
+            qat_dict[this_str] = [qat_dict[this_str]] + [jaw_calc_pos * cor_fac]
     else:
         qat_dict[this_str] = [jaw_calc_pos * cor_fac]
 
@@ -392,7 +403,7 @@ zip_path = FILE.name  # For use in QA Track with file upload
 
 tmp_dir = tempfile.mkdtemp()
 
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+with zipfile.ZipFile(zip_path, "r") as zip_ref:
     zip_ref.extractall(tmp_dir)
 
 __D_DIR = tmp_dir
@@ -410,11 +421,12 @@ img_fig_list = []  # List of images, for plotting later
 
 # For each image not used to find the MV CAX
 for ff in sorted(os.listdir(__D_DIR))[2:]:
-
     ds = dcmread(os.path.join(__D_DIR, ff))  # Get dicom
 
     # Get image, normalize, and store max value
     image = ds.pixel_array
+    image = (image - np.min(image)) / (np.max(image) - np.min(image))
+    image = suppress_hot_pixels(im=image)
     image = (image - np.min(image)) / (np.max(image) - np.min(image))
     img_max = np.max(image)
 
@@ -426,22 +438,23 @@ for ff in sorted(os.listdir(__D_DIR))[2:]:
         this_CAX = beam_CAX
 
     # Find the jaw edges in the image
-    (x1_edge, x1_edge_mm,
-     x2_edge, x2_edge_mm,
-     y1_edge, y1_edge_mm,
-     y2_edge, y2_edge_mm) = _find_all_edges(image=image, ds=ds,
-                                            beam_CAX=this_CAX,
-                                            img_max=img_max)
+    (
+        x1_edge,
+        x1_edge_mm,
+        x2_edge,
+        x2_edge_mm,
+        y1_edge,
+        y1_edge_mm,
+        y2_edge,
+        y2_edge_mm,
+    ) = _find_all_edges(image=image, ds=ds, beam_CAX=this_CAX, img_max=img_max)
 
     # Get jaw positions from plan
     x1, x2, y1, y2 = _get_dcm_jaw_pos(ds=ds)
 
     # Store the jaw positions here
     jaw_pos_list.append(
-        (
-            (x1, x2, y1, y2),
-            (x1_edge_mm, x2_edge_mm, y1_edge_mm, y2_edge_mm)
-        )
+        ((x1, x2, y1, y2), (x1_edge_mm, x2_edge_mm, y1_edge_mm, y2_edge_mm))
     )
 
     # Get pixel spacing from DICOM (assumed format: [row_spacing, col_spacing])
@@ -451,75 +464,87 @@ for ff in sorted(os.listdir(__D_DIR))[2:]:
     fig = plt.figure(figsize=(8, 8))
     fig.suptitle(f"Image {cc+1}", fontsize=16, y=0.96)
 
-    plt.imshow(image, cmap='gray')
+    plt.imshow(image, cmap="gray")
 
     center_pixel = np.array([image.shape[0] / 2.0, image.shape[1] / 2.0])
 
     # Draw a vertical red line at the detected x-edge position.
-    plt.axvline(x=x1_edge, color='y', linestyle='-',
-                linewidth=2, label='Detected X1-Edge')
-    plt.text(x1_edge + 50, float(center_pixel[0]),
-             f"Meas: {x1_edge_mm:.2f} mm\nPlan: {x1:.2f} mm\nDiff: "
-             f"{(x1_edge_mm - x1):.2f} mm",
-             color='red',
-             fontsize=9,
-             bbox=dict(
-                 facecolor='white',
-                 alpha=0.9,
-                 edgecolor='black',
-                 boxstyle='round',
-             )
-             )
+    plt.axvline(
+        x=x1_edge, color="y", linestyle="-", linewidth=2, label="Detected X1-Edge"
+    )
+    plt.text(
+        x1_edge + 50,
+        float(center_pixel[0]),
+        f"Meas: {x1_edge_mm:.2f} mm\nPlan: {x1:.2f} mm\nDiff: "
+        f"{(x1_edge_mm - x1):.2f} mm",
+        color="red",
+        fontsize=9,
+        bbox=dict(
+            facecolor="white",
+            alpha=0.9,
+            edgecolor="black",
+            boxstyle="round",
+        ),
+    )
     # Draw a vertical red line at the detected x-edge position.
-    plt.axvline(x=x2_edge, color='y', linestyle='--',
-                linewidth=2, label='Detected X2-Edge')
-    plt.text(x2_edge + 50, float(center_pixel[0]),
-             f"Meas: {x2_edge_mm:.2f} mm\nPlan: {x2:.2f} mm\nDiff: "
-             f"{(x2_edge_mm - x2):.2f} mm",
-             color='red',
-             fontsize=9,
-             bbox=dict(
-                 facecolor='white',
-                 alpha=0.9,
-                 edgecolor='black',
-                 boxstyle='round',
-             )
-             )
+    plt.axvline(
+        x=x2_edge, color="y", linestyle="--", linewidth=2, label="Detected X2-Edge"
+    )
+    plt.text(
+        x2_edge + 50,
+        float(center_pixel[0]),
+        f"Meas: {x2_edge_mm:.2f} mm\nPlan: {x2:.2f} mm\nDiff: "
+        f"{(x2_edge_mm - x2):.2f} mm",
+        color="red",
+        fontsize=9,
+        bbox=dict(
+            facecolor="white",
+            alpha=0.9,
+            edgecolor="black",
+            boxstyle="round",
+        ),
+    )
     # Draw a horizontal red line at the detected y-edge position.
-    plt.axhline(y=y1_edge, color='red', linestyle='-',
-                linewidth=2, label='Detected Y1-Edge')
-    plt.text(float(center_pixel[1]), y1_edge + 50,
-             f"Meas: {y1_edge_mm:.2f} mm\nPlan: {y1:.2f} mm\nDiff: "
-             f"{(y1_edge_mm - y1):.2f} mm",
-             color='red',
-             fontsize=9,
-             bbox=dict(
-                 facecolor='white',
-                 alpha=0.9,
-                 edgecolor='black',
-                 boxstyle='round',
-             )
-             )
+    plt.axhline(
+        y=y1_edge, color="red", linestyle="-", linewidth=2, label="Detected Y1-Edge"
+    )
+    plt.text(
+        float(center_pixel[1]),
+        y1_edge + 50,
+        f"Meas: {y1_edge_mm:.2f} mm\nPlan: {y1:.2f} mm\nDiff: "
+        f"{(y1_edge_mm - y1):.2f} mm",
+        color="red",
+        fontsize=9,
+        bbox=dict(
+            facecolor="white",
+            alpha=0.9,
+            edgecolor="black",
+            boxstyle="round",
+        ),
+    )
     # Draw a horizontal red line at the detected y-edge position.
-    plt.axhline(y=y2_edge, color='red', linestyle='--',
-                linewidth=2, label='Detected Y2-Edge')
-    plt.text(float(center_pixel[1]), y2_edge + 50,
-             f"Meas: {y2_edge_mm:.2f} mm\nPlan: {y2:.2f} mm\nDiff: "
-             f"{(y2_edge_mm - y2):.2f} mm",
-             color='red',
-             fontsize=9,
-             bbox=dict(
-                 facecolor='white',
-                 alpha=0.9,
-                 edgecolor='black',
-                 boxstyle='round',
-             )
-             )
+    plt.axhline(
+        y=y2_edge, color="red", linestyle="--", linewidth=2, label="Detected Y2-Edge"
+    )
+    plt.text(
+        float(center_pixel[1]),
+        y2_edge + 50,
+        f"Meas: {y2_edge_mm:.2f} mm\nPlan: {y2:.2f} mm\nDiff: "
+        f"{(y2_edge_mm - y2):.2f} mm",
+        color="red",
+        fontsize=9,
+        bbox=dict(
+            facecolor="white",
+            alpha=0.9,
+            edgecolor="black",
+            boxstyle="round",
+        ),
+    )
 
     cc += 1
-    plt.xlabel('Pixel Column')
-    plt.ylabel('Pixel Row')
-    plt.legend(loc='lower right')
+    plt.xlabel("Pixel Column")
+    plt.ylabel("Pixel Row")
+    plt.legend(loc="lower right")
     plt.tight_layout()
     img_fig_list.append(fig)
     # plt.close()
@@ -528,12 +553,11 @@ for ff in sorted(os.listdir(__D_DIR))[2:]:
 
 # Make summary_data for plt.table() and dict for QA Track reporting
 summary_data = []
-qat_dict = dict()
+ij_images = dict()
 for ii in range(len(jaw_pos_list)):
-    (
-        (x1, x2, y1, y2),
-        (x1_edge_mm, x2_edge_mm, y1_edge_mm, y2_edge_mm)
-    ) = jaw_pos_list[ii]
+    ((x1, x2, y1, y2), (x1_edge_mm, x2_edge_mm, y1_edge_mm, y2_edge_mm)) = jaw_pos_list[
+        ii
+    ]
 
     summary_data.append(
         [
@@ -542,43 +566,33 @@ for ii in range(len(jaw_pos_list)):
             "%.2f" % (x2_edge_mm - x2),
             "%.2f" % (y1_edge_mm - y1),
             "%.2f" % (y2_edge_mm - y2),
-         ]
+        ]
     )
 
-    qat_dict = _make_qat_dict_entry(
-        jaw_dcm_pos=x1,
-        jaw_calc_pos=x1_edge_mm,
-        qat_dict=qat_dict,
-        jaw_str="X1"
+    ij_images = _make_qat_dict_entry(
+        jaw_dcm_pos=x1, jaw_calc_pos=x1_edge_mm, qat_dict=ij_images, jaw_str="X1"
     )
-    qat_dict = _make_qat_dict_entry(
-        jaw_dcm_pos=x2,
-        jaw_calc_pos=x2_edge_mm,
-        qat_dict=qat_dict,
-        jaw_str="X2"
+    ij_images = _make_qat_dict_entry(
+        jaw_dcm_pos=x2, jaw_calc_pos=x2_edge_mm, qat_dict=ij_images, jaw_str="X2"
     )
-    qat_dict = _make_qat_dict_entry(
-        jaw_dcm_pos=y1,
-        jaw_calc_pos=y1_edge_mm,
-        qat_dict=qat_dict,
-        jaw_str="Y1"
+    ij_images = _make_qat_dict_entry(
+        jaw_dcm_pos=y1, jaw_calc_pos=y1_edge_mm, qat_dict=ij_images, jaw_str="Y1"
     )
-    qat_dict = _make_qat_dict_entry(
-        jaw_dcm_pos=y2,
-        jaw_calc_pos=y2_edge_mm,
-        qat_dict=qat_dict,
-        jaw_str="Y2"
+    ij_images = _make_qat_dict_entry(
+        jaw_dcm_pos=y2, jaw_calc_pos=y2_edge_mm, qat_dict=ij_images, jaw_str="Y2"
     )
 
-for ii in list(qat_dict.keys()):
+for ii in list(ij_images.keys()):
     if "20" in ii:
-        qat_dict[ii] = qat_dict[ii][
-            np.argmax(np.abs(np.array(qat_dict[ii]) - 20))] / 10
+        ij_images[ii] = (
+            ij_images[ii][np.argmax(np.abs(np.array(ij_images[ii]) - 20))] / 10
+        )
     elif "-20" in ii:
-        qat_dict[ii] = qat_dict[ii][
-            np.argmax(np.abs(np.array(qat_dict[ii]) + 20))] / 10
+        ij_images[ii] = (
+            ij_images[ii][np.argmax(np.abs(np.array(ij_images[ii]) + 20))] / 10
+        )
     else:
-        qat_dict[ii] = qat_dict[ii][0] / 10
+        ij_images[ii] = ij_images[ii][0] / 10
 
 # Define column headers for the summary table.
 col_labels = [
@@ -586,11 +600,11 @@ col_labels = [
     "X1 Diff (mm)",
     "X2 Diff (mm)",
     "Y1 Diff (mm)",
-    "Y2 Diff (mm)"
+    "Y2 Diff (mm)",
 ]
 # Create a new figure for the summary.
 fig_summary = plt.figure(figsize=(8, 11))
-plt.axis('off')
+plt.axis("off")
 plt.title("Jaw Position Differences Summary", fontsize=14, pad=20)
 
 max_diff_x1 = np.max([abs(float(row[1])) for row in summary_data])
@@ -604,14 +618,12 @@ extra_text = (
     f"X1: {max_diff_x1:.2f} mm, X2: {max_diff_x2:.2f} mm, "
     f"Y1: {max_diff_y1:.2f} mm, Y2: {max_diff_y2:.2f} mm"
 )
-plt.figtext(0.5, 0.85, extra_text, wrap=True, horizontalalignment='center',
-            fontsize=12)
+plt.figtext(0.5, 0.85, extra_text, wrap=True, horizontalalignment="center", fontsize=12)
 
 # Create the table inside the figure.
-table = plt.table(cellText=summary_data,
-                  colLabels=col_labels,
-                  loc='center',
-                  cellLoc='center')
+table = plt.table(
+    cellText=summary_data, colLabels=col_labels, loc="center", cellLoc="center"
+)
 table.auto_set_font_size(False)
 table.set_fontsize(10)
 table.scale(1, 2)  # Adjust the row heights
